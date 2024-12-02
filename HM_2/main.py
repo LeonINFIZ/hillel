66914677149d0f5ccbc61713465830559799209c
Артем Топалов <feofan.neyromonah@gmail.com>
2024-12-02 21:44:20 +0200

    + PEP 8

--- HM_2/main.py
+++ HM_2/main.py
@@ -3,8 +3,8 @@ num = int(input("Введіть 4-х значне число: ")) # або num =
 while (num < 1000 or num > 9999):
     num = int(input("Введіть 4-х значне число: "))
 
-print(num//1000) # 1
-print(num%10**3 // 10**2) # 2
-print(num%10**2 // 10) # 3
-print(num%10) # 4
+print(num // 1000) # 1
+print(num % 10 ** 3 // 10 ** 2) # 2
+print(num % 10 ** 2 // 10) # 3
+print(num % 10) # 4
 
