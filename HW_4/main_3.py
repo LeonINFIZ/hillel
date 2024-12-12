065e68d9d8246f36f530abede5636d7ec94d9c1e
Артем Топалов <feofan.neyromonah@gmail.com>
2024-12-12 12:37:49 +0200

    HW 4

--- HW_4/main_3.py
+++ HW_4/main_3.py
@@ -4,17 +4,17 @@ def get_random_list():
     return [randint(1,100) for i in range(randint(3, 10))]
 
 
-def my_fuction(my_list):
+def my_function(my_list):
     print(my_list, end=" == ")
 
     new_list = [my_list[0], my_list[2], my_list[-2]]
 
     print(new_list)
 
-my_fuction([1, 2, 3, 4, 5, 6, 7, 9])    # Приклад з завдання
-my_fuction([1, 1, 2, 1])                # Приклад з завдання
-my_fuction([6, 3, 7])                   # Приклад з завдання
+my_function([1, 2, 3, 4, 5, 6, 7, 9])    # Приклад з завдання
+my_function([1, 1, 2, 1])                # Приклад з завдання
+my_function([6, 3, 7])                   # Приклад з завдання
 
 print()
 
-my_fuction(get_random_list())
+my_function(get_random_list())
