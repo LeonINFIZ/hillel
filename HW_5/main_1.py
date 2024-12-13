114042f0b458004a1978022b57bb9126d933efa7
Артем Топалов <feofan.neyromonah@gmail.com>
2024-12-13 16:48:16 +0200

    HW_5

---
+++ HW_5/main_1.py
@@ -0,0 +1,21 @@
+import keyword
+import string
+
+punctuation = set(string.punctuation)
+
+print(punctuation)
+punctuation.remove('_')
+
+print(keyword.kwlist)
+
+text = input("\nВведіть ім'я змінної: ")
+
+if text in keyword.kwlist or len(text.split()) > 1 or text[0].isdigit():
+    print("False")
+else:
+    for i in range(len(text)):
+        if text[i] in punctuation or text[i].isupper() or (text[i] == "_" and text[i+1] == "_"):
+            print("False")
+            break
+    else:
+        print("True")
